d8168aeffbe54c3c795c0188de1e5135c8a7ea75
YUICHI061 <35321011+YUICHI061@users.noreply.github.com>
2018-05-08 23:08:08 -0700

    Initial commit

---
+++ bike.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 23:05:07 2018
+
+@author: hayas
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train = pd.read_csv('E:/Python Github/Bike - Kaggle/train.csv')
+test = pd.read_csv('E:/Python Github/Bike - Kaggle/test.csv')
